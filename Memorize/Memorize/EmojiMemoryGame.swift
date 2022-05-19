bce60f4cf147f4d93a0ef134dabbe3aff4e27985
Christopher Koski <82855685+ChrisKoskiii@users.noreply.github.com>
2022-05-19 06:48:31 -0400

    made UI reactive

--- Memorize/Memorize/EmojiMemoryGame.swift
+++ Memorize/Memorize/EmojiMemoryGame.swift
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-class EmojiMemoryGame {
+class EmojiMemoryGame: ObservableObject {
   static let emojis = ["✈️", "🚀", "🚗", "🛵", "🚑", "🛬", "🛳", "🚍", "🚜", "🛴", "🚕", "🚔", "🚚", "🚁", "🚢", "🚒", "🚂", "🛺", "🚤"]
   
   static func createMemoryGame() -> MemoryGame<String> {
@@ -16,9 +16,15 @@ class EmojiMemoryGame {
     }
   }
   
-  private var model: MemoryGame<String> = createMemoryGame()
+  @Published private var model: MemoryGame<String> = createMemoryGame()
   
   var cards: Array<MemoryGame<String>.Card> {
-    return model.cards
+    model.cards
+  }
+  
+  // MARK: Intent(s)
+  
+  func choose(_ card: MemoryGame<String>.Card) {
+    model.choose(card)
   }
 }
